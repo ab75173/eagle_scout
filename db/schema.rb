32c26db62a5bd7a060c066124a62946e6f2ee66c
ab75173 <adam.p.barrett@gmail.com>
2014-07-11 11:38:21 -0400

    db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140710154226) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: true do |t|
     t.string   "name"
     t.text     "description"
@@ -33,7 +36,7 @@ ActiveRecord::Schema.define(version: 20140710154226) do
     t.string "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
